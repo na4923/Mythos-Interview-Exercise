7d4b5fcd31a7c4e56ecdb18daabd94b62bc3d506
na4923 <na4923@rit.edu>
2026-03-03 21:55:03 -0500

    basic setup of the files

---
+++ src/generator.js
@@ -0,0 +1,6 @@
+import PDFDocument from 'pdfkit';
+import fs from 'node:fs'
+
+export default async function generate_pdf() {
+  const doc = new PDFDocument();
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const filename = "data/" + process.argv[2];
+
+export default async function make_cover_letters(filename) {
+
+}
+make_cover_letters(filename)
